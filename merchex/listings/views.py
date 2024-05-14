c0f6334bbed076a7ce9b740e05d0db848bd453ae
paruyrm <paruyrm@yahoo.com>
2024-05-14 16:12:29 +0200

    a
    d

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def hello(render):
-     return HttpResponse('<h1>Hello Paruyr!</h1>')
+from listings.models import Band
+
+def hello(request):
+     bands = Band.objects.all ()
+     return HttpResponse(f"""
+         <h1>Hello Django !</h1>
+         <p>Mes groupes préférés sont :<p>
+         <ul>
+              <li>{bands[0].name}</li>
+              <li>{bands[1].name}</li>
+         </ul>""")
  
 def about(request):
     return HttpResponse('<h1>À propos de nous</h1> <p>Nous adorons merch !</p>')
