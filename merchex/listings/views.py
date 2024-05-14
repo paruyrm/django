ba3fa07979fcb822972dbfc8a223d7fd6703d4c3
paruyrm <paruyrm@yahoo.com>
2024-05-14 14:55:47 +0200

    a
    a

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 def hello(render):
      return HttpResponse('<h1>Hello Paruyr!</h1>')
-
+ 
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
 # Create your views here.
